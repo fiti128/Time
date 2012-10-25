2d7de412c7974f2a59907160448644789c327cdc
user <tr-legres@yandex.ru>
2012-10-25 12:24:42 +0400

    adding apache license

--- src/main/java/ru/retbansk/mail/Main.java
+++ src/main/java/ru/retbansk/mail/Main.java
@@ -1,12 +1,30 @@
+/*
+ * Copyright 2002-2012 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ru.retbansk.mail;
-
-
-
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-
+/**
+ * Main class that starts everything. 
+ * <p>First point of the program
+ *
+ * @author Siarhei Yanusheuski
+ * @since 25.10.2012
+ */
 public class Main {
 
 	/**
@@ -14,21 +32,9 @@ public class Main {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-//		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		new Thread(new Runnable() {
-			public void run() {
+
 				new ClassPathXmlApplicationContext("scheduledConfig.xml", Main.class);
-			}
-		}).start();
-//		new ClassPathXmlApplicationContext("scheduledConfig.xml", Main.class);
-//		new ReadEmailAndConvertToXmlSpringAnnotatedImpl().execute();
-//		new Thread(new Runnable() {
-//										public void run() {
-//											Scanner sc = new Scanner(System.in);
-//											if (sc.next().equals("5")) System.exit(0);
-//											sc.close();
-//										}
-//		}).start();
+
 
 	}
 
